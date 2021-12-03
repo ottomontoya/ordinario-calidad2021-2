019e2fab5b73a44794c2759c0f98fcd05f0009d1
Otto Montoya <montoyaotto@icloud.com>
2021-12-02 18:10:35 -0600

    agregacion de createBefore en setup de mernCrudTest para tener un usuario antes de todo, s/o mike lezama

--- src/test/java/com/anahuac/calidad/funcionales/mernCrudTest.java
+++ src/test/java/com/anahuac/calidad/funcionales/mernCrudTest.java
@@ -28,6 +28,7 @@ public class mernCrudTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         js = (JavascriptExecutor) driver;
+        createBefore();
     }
 
     @Test
@@ -91,9 +92,6 @@ public class mernCrudTest {
         String tag = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/div")).getText();
         assertThat("Nice one!", is(tag));
     }
-
-
-
     @Test
     public void deleteTest() throws Exception{
         driver.get("https://mern-crud.herokuapp.com/");
@@ -115,6 +113,31 @@ public class mernCrudTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public void createBefore(){
+        int numRandom = new Random().nextInt();
+
+        driver.get("https://mern-crud.herokuapp.com/");
+        driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+        driver.findElement(By.name("name")).click();
+        driver.findElement(By.name("name")).clear();
+        driver.findElement(By.name("name")).sendKeys("otto montoya");
+        driver.findElement(By.name("email")).clear();
+
+        // Aqui agrego el numRandom
+        driver.findElement(By.name("email")).sendKeys("ottomontoya" + String.valueOf(numRandom) +"@gmail.com");
+
+        driver.findElement(By.name("age")).clear();
+        driver.findElement(By.name("age")).sendKeys("21");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+
+        wait(3);
+
+        String tag = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/div")).getText();
+        assertThat("Nice one!", is(tag));
     }
 
     @After
